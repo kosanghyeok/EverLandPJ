29658a592d4e88698b3954462ec4f5b105aecd0b
고상혁 <kosang5719@gmail.com>
2021-06-29 15:40:44 +0900

    3개의 파일로 최종본.

--- src/EverlandMain.java
+++ src/EverlandMain.java
@@ -7,37 +7,38 @@ import java.util.Scanner;
 
 
 public class EverlandMain {
-	public static int addOrder = 0, Prior;
-	public static String ticketType, registNumber;
+	
 	public static void main(String[] args) {
 		
 		EverlandProcess everProcess = new EverlandProcess(); 
 		ArrayListControlClass everArray = new ArrayListControlClass();
 		everProcess.whatToday();
-      while(true) {
-    	  everArray.clearALL();
-      do {
+	
+		
+	while(true) {
+		 everArray.clearALL();
+	 do {
     	  everProcess.inputDate();
     	  everProcess.Pricetag();
-    	  System.out.println(ticketType);
+    	  System.out.println(everProcess.ticketType);
   		  System.out.println("ㅡ");
-  		everProcess.putID();
-  		  if (registNumber.equals("0")) {continue;}
-  		everProcess.HowBirth();
-  		everProcess.ManyCount();
-  		everProcess.choosePrior();
-  		  if(Prior == 0) {continue;}
+  		  everProcess.putID();
+  		  if (everProcess.registNumber.equals("0")) {continue;}
+  		  everProcess.HowBirth();
+  		  everProcess.ManyCount();
+  		  everProcess.choosePrior();
+  		  if(everProcess.Prior == 0) {continue;}
   		  System.out.println("ㅡ");
-  		everProcess.moreORnot();
-  		 everArray.Arrayadd(everProcess.ticketType, everProcess.age,
+  		  everProcess.moreORnot();
+  		  everArray.Arrayadd(everProcess.ticketType, everProcess.age,
   				  			everProcess.Count, everProcess.Price, everProcess.b);
 		  System.out.println("감사합니다.");
 	      System.out.println("=============== 에버랜드 =================");
-      	}while(addOrder == 1);
-      everArray.Result();   
-  		 break;
-      }
-     
+      	}while(everProcess.addOrder == 1);
+      	  everArray.Result();   
+      	  break;
 	}
+     
+}
 
 }
